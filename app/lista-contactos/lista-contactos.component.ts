248522c1d9a66149e16ab61289b2823bdaa7a6b4
jaarboledam <jaarboledam@gmail.com>
2016-11-14 00:39:27 -0500

    Se añade componente para mostrar el detalle de contacto

--- app/lista-contactos/lista-contactos.component.ts
+++ app/lista-contactos/lista-contactos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Contacto } from '../entidades/contacto';
 
 @Component({
@@ -9,4 +9,9 @@ export class ListaContactosComponent {
     // Se usa el decorador Input para indicar que espera la lista,
     // la cuál se está indicando cuando se declara el selector (<lista-contactos [contactos]="listaContactos">)
     @Input() contactos: Contacto[];
+    @Output() seleccionado: EventEmitter<Contacto> = new EventEmitter();
+
+    notificarContactoSeleccionado(contacto: Contacto): void {
+        this.seleccionado.emit(contacto);
+    }
 }
